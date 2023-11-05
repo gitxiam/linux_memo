a1725608689d4b9b9e2c8878be83f9116f0d3b6d
Sergey <sergey@windows10.com>
2023-11-05 23:34:11 +0300

    add ps mkdir functions and other

--- dunno_how_named_this.sh
+++ dunno_how_named_this.sh
@@ -43,3 +43,52 @@ ls -l /sys/class/net/
 #see how to use
 # https://www.cyberciti.biz/faq/linux-list-network-interfaces-names-command/
 
+
+#directory info via ls
+ls -ld
+
+#info about inode and all files
+ls -ai
+
+#chain not created directories
+mkdir -p testdirectory1/testdirectory2/testdirectory3
+
+#Regex crossword lol for training
+#https://regexcrossword.com/
+
+# ps for process show in terminal
+ps aux #my favorite
+ps au #users process
+ps f #childrens and parents flows
+
+
+#Below are some additional operations with variables:
+${#array[@]} - the size of the array.
+${{!arr[@]} - get array indexes
+arr[0]=value - overwrites the first element of the array
+arr+=(value) - attaching the value to the array
+str=$(ls) - saving the output of the ls command as a string
+${arr[@]:s:n} - get array elements starting from the element with index s up to the element with index s+(n-1).
+${VAR:pos} - substring from pos position
+${VAR:pos:len} - substring from position pos with length length length
+${VAR?err_msg} - checks if VAR is defined, its value is returned, otherwise print err_msg
+${#VAR} - returns the number of characters(length) of the variable value.
+
+# functions in bash
+#!/bin/bash
+clear
+
+function testFunc {
+
+for VAR in 1 8 3 4 6 7 1 9 30; do
+if (( VAR % 2 == 1 )); then
+continue; fi
+echo $VAR
+done
+
+}
+
+# and eecute in script, don't forget!
+testFunc
+echo "script finished"
+
